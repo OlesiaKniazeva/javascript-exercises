f312d383a154cdd9e600146e34a92e103ee5c2dd
Olesia Kniazeva <knyazevaolesiya@gmail.com>
2024-01-13 14:54:51 +0800

    complete second exersize

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (stringName, repetitionAmount) {
+  if (repetitionAmount < 0) {
+    return "ERROR";
+  }
+  return stringName.repeat(repetitionAmount);
 };
+
+console.log(repeatString("hey", 0));
+console.log(`|${' '.repeat(10)}|`);
 
 // Do not edit below this line
 module.exports = repeatString;
