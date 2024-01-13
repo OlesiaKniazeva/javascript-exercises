ccc44c915ed03a0cb61c867408a913fa855f784f
Olesia Kniazeva <knyazevaolesiya@gmail.com>
2024-01-13 16:19:24 +0800

    complete fourth task

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
 
+  return array.filter(function(word) {
+    return !args.includes(word);
+  })
 };
+
+let res = removeFromArray([1, 2, 3], 1, 2);
+console.log(res);
 
 // Do not edit below this line
 module.exports = removeFromArray;
