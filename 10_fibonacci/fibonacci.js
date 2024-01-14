b56aecdc0ea349f3c616c7217d2bb1cf8b419262
Olesia Kniazeva <knyazevaolesiya@gmail.com>
2024-01-14 19:12:50 +0800

    complete tenth task

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (arg) {
+  let number = Number(arg);
 
+  if (number === NaN || number < 0) {
+    return "OOPS";
+  }
+  if (number < 2) {
+    return number;
+  }
+
+  let fibonacciIndex = 1;
+
+  let result = 0;
+  let firstNumber = 0;
+  let secondNumber = 1;
+  while (number > fibonacciIndex) {
+    result = firstNumber + secondNumber;
+    firstNumber = secondNumber;
+    secondNumber = result;
+    fibonacciIndex++;
+  }
+  return result;
 };
 
 // Do not edit below this line
