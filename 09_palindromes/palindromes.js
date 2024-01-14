cde19ddf809cbe978c7dbfa3b3ceeb9be5f8b9a7
Olesia Kniazeva <knyazevaolesiya@gmail.com>
2024-01-14 18:49:11 +0800

    complete nineth task

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (arg) {
+  arg = arg.toLowerCase().replace(/[^a-z0-9]/g, '');
+  console.log(arg);
+  let reverse = arg.split('').reverse().join('');
+  return arg === reverse;
 };
+
+console.log(palindromes("A car, a man, a maraca"));
 
 // Do not edit below this line
 module.exports = palindromes;
