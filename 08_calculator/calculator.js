6e9da25fb71e2a828b4f1ed2101889e7f255a0ee
Olesia Kniazeva <knyazevaolesiya@gmail.com>
2024-01-14 18:15:26 +0800

    complete eight task

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,43 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+  return nums.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
 };
 
-const multiply = function() {
-
+const multiply = function (nums) {
+  return nums.reduce(
+    (accumulator, currentValue) => accumulator * currentValue,
+    1
+  );
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  return factorialize(number);
 };
+
+function factorialize(number) {
+  if (number === 0) {
+    return 1;
+  }
+
+  if (number <= 2) {
+    return number;
+  }
+  return number * factorialize(number - 1);
+}
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +46,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
