d5693ed0e380768c83882e889674deffd83863c9
Olesia Kniazeva <knyazevaolesiya@gmail.com>
2024-01-14 17:49:29 +0800

    complete seventh task

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(celsiusTemperature) {
+  return round((celsiusTemperature - 32) / 1.8);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(fahrenheitTemperature) {
+  return round((fahrenheitTemperature * 1.8) + 32);
 };
+
+function round(number) {
+  return Math.round(number * 10) / 10;
+}
+
+console.log(convertToCelsius(32));
 
 // Do not edit below this line
 module.exports = {
