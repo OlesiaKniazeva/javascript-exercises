61f7ba355507e344e892f53b5650ffd3cd1549da
Olesia Kniazeva <knyazevaolesiya@gmail.com>
2024-01-14 17:18:26 +0800

    finish sixth exersize

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const LEAP_YEAR = true;
 
+const leapYears = function (year) {
+  /* years divisible by 4 are not leap years */
+  if (year % 4 !== 0) {
+    return !LEAP_YEAR;
+  }
+
+  /* special case, years which are divisible 
+  by 100 and not divisible by 400 are not leap years */
+  if (year % 100 === 0 && !(year % 400 === 0)) {
+    return !LEAP_YEAR;
+  }
+  return LEAP_YEAR;
 };
 
 // Do not edit below this line
